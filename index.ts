22579b435cc3761b6aeb70adf16aacf9c6fd0e41
FujiwaraKen <fjwr1024@gmail.com>
2023-10-30 06:33:37 +0900

    add script

---
+++ index.ts
@@ -0,0 +1,33 @@
+import * as dotenv from "dotenv";
+import OpenAI, { ClientOptions } from "openai";
+
+dotenv.config();
+
+const apiKey = process.env.OPENAI_API_KEY;
+if (!apiKey) {
+  throw new Error("APIキーが設定されていません。");
+}
+
+const clientOptions: ClientOptions = {
+  apiKey: apiKey,
+};
+
+const openai = new OpenAI(clientOptions);
+
+async function sendMessage() {
+  try {
+    const completionRequest = {
+      model: "gpt-3.5-turbo",
+      prompt: "Hello. Test AI Chatbot.",
+    };
+
+    const response = await openai.completions.create(completionRequest);
+
+    // console.log(response.data.choices[0].text.trim());
+    console.log(response);
+  } catch (error) {
+    console.error("error:", error);
+  }
+}
+
+sendMessage();
