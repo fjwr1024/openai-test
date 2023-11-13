f21ea577ba015255bb6bb2f7ec5bb30e42053440
FujiwaraKen <fjwr1024@gmail.com>
2023-11-13 19:18:18 +0900

    image upload

--- image.ts
+++ image.ts
@@ -18,10 +18,24 @@ const clientOptions: ClientOptions = {
 const openai = new OpenAI(clientOptions);
 
 async function main() {
-  const file = await openai.files.create({
-    file: fs.createReadStream("mydata.jsonl"),
-    purpose: "fine-tune",
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          { type: "text", text: "What are in these images? Is there any difference between them?" },
+          {
+            type: "image_url",
+            image_url: {
+              url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            },
+          },
+        ],
+      },
+    ],
   });
+  console.log(response);
+  console.log(response.choices[0]);
 }
-
 main();
