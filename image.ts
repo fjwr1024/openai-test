bc5868d6ca8380e08ec4cd665723115817d7d065
FujiwaraKen <fjwr1024@gmail.com>
2023-11-16 20:08:18 +0900

    fix

--- image.ts
+++ image.ts
@@ -7,35 +7,52 @@ import * as fs from 'fs';
 import util from 'util';
 import openaiClient from './shared/openapi-client';
 
-function encodeImageToBase64(filePath: any) {
-  return fs.readFileSync(filePath, { encoding: 'base64' });
+function encodeImageToBase64(filePath: string) {
+  try {
+    return fs.readFileSync(filePath, { encoding: 'base64' });
+  } catch (error) {
+    console.error('Failed to read the image file:', error);
+    process.exit(1);
+  }
 }
 
-async function uploadImage() {
-  const imagePath = './images/sample.png';
+async function uploadImage(imagePath: string, inputText: string) {
   const base64Image = encodeImageToBase64(imagePath);
 
-  const inputText = process.argv[2];
-
-  const response = await openaiClient.chat.completions.create({
-    model: 'gpt-4-vision-preview',
-    max_tokens: 500,
-    messages: [
-      {
-        role: 'user',
-        content: [
-          { type: 'text', text: inputText },
-          {
-            type: 'image_url',
-            image_url: {
-              url: `data:image/png;base64,${base64Image}`,
+  try {
+    const response = await openaiClient.chat.completions.create({
+      model: 'gpt-4-vision-preview',
+      max_tokens: 500,
+      messages: [
+        {
+          role: 'user',
+          content: [
+            { type: 'text', text: inputText },
+            {
+              type: 'image_url',
+              image_url: {
+                url: `data:image/png;base64,${base64Image}`,
+              },
             },
-          },
-        ],
-      },
-    ],
-  });
-  console.log(util.inspect(response, { showHidden: false, depth: null, colors: true }));
-  console.log(util.inspect(response.choices[0], { showHidden: false, depth: null, colors: true }));
+          ],
+        },
+      ],
+    });
+    console.log(util.inspect(response, { showHidden: false, depth: null, colors: true }));
+    console.log(
+      util.inspect(response.choices[0], { showHidden: false, depth: null, colors: true })
+    );
+  } catch (error) {
+    console.error('Failed to send the request to OpenAI:', error);
+    process.exit(1);
+  }
 }
-uploadImage();
+
+const inputText = process.argv[2];
+if (!inputText) {
+  console.error('Please provide the text for analysis.');
+  process.exit(1);
+}
+
+const imagePath = './images/sample.png';
+uploadImage(imagePath, inputText);
