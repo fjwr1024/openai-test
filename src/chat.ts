2de5f863ed77f8b1a15bffa1b384cd5c4d5d4c36
FujiwaraKen <fjwr1024@gmail.com>
2023-11-24 21:26:14 +0900

    linter comment fixed

--- src/chat.ts
+++ src/chat.ts
@@ -1,15 +1,17 @@
 import openaiClient from './shared/openapi-client';
 
-async function sendMessage() {
+export const sendMessage = async (inputText: string) => {
   const completion = await openaiClient.chat.completions.create({
     messages: [{ role: 'system', content: inputText }],
     model: 'gpt-4-1106-preview',
     max_tokens: 500,
   });
 
-  console.log(completion);
-  console.log(completion.choices[0].message);
-}
+  console.log('# completion', completion);
+  console.log('# message', completion.choices[0].message);
+};
 
 const inputText = process.argv[2];
-sendMessage();
+sendMessage(inputText)
+  .then(() => console.log('Message sent successfully'))
+  .catch(error => console.error('Failed to send message:', error));
